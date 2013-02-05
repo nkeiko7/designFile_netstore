7c90bd967c604bce72c9118476f369783a6a1a67
keiko_nishiwaki <nkeiko7@gmail.com>
2013-02-05 17:42:57 +0900

    2013/02/05 差分をコミット

---
+++ shared/js/sc/code_to_paste.js
@@ -0,0 +1,4 @@
+if(location.pathname.toLowerCase().match(/\/store\/cmdty\/search/) && $(".message").size() > 0){s.events=s.apl(s.events,"event2",",",1);}
+if(document.body.className=="store browse"&&document.body.id=="item"){s.products=";" + janCode;s.events="event3,prodView";}
+/************* DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
+var s_code=s.t();if(s_code)document.write(s_code)
